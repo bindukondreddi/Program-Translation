8f809b64ccb6c07e193cc2a7b06f7da6279d3d4e
bindukondreddi <bindukondreddi@gmail.com>
2017-06-05 21:02:29 -0400

    Update lexer.h

--- src/lexer.h
+++ src/lexer.h
@@ -1,16 +1,3 @@
-/*
-
-	Name: Adrian J. Paparelli
-	Class: CSCI 5220
-	Session: Spring 2016
-
-	Description: lexer.h contains variable definitiosn for use by the lexical analyzer
-	in the absence of Bison.
-
-	Change Log:
-	2016-02-08: Initial Revision
-
-*/
 
 #ifndef LEXER_H
 #define LEXER_H
