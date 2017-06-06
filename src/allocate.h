21a343fe69d4c699d96b3b9afdcb0e57bb16eeab
bindukondreddi <bindukondreddi@gmail.com>
2017-06-05 21:00:20 -0400

    Update allocate.h

--- src/allocate.h
+++ src/allocate.h
@@ -1,17 +1,3 @@
-/*
-
-	Name: Adrian J. Paparelli
-	Class: CSCI 5220
-	Session: Spring 2016
-
-	Description: allocate.h defines all functions used in allocating
-		memory from the heap, copied from assignment #1 description
-
-	Change Log:
-	2016-11-14: Initial Revision
-
-*/
-
 #ifndef ALLOCATE_H
 #define ALLOCATE_H
 
