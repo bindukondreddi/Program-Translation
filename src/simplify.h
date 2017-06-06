caf14107ae2a281c68be875fa51986c3e6c961c6
bindukondreddi <bindukondreddi@gmail.com>
2017-06-05 21:03:39 -0400

    Update simplify.h

--- src/simplify.h
+++ src/simplify.h
@@ -1,16 +1,4 @@
-/*
 
-	Name: Adrian J. Paparelli
-	Class: CSCI 5220
-	Session: Spring 2016
-
-	Description: simplify.h contains all function definitions used by the SFL simplifier. 
-	Function descriptions will follow to explain each funtion.
-
-	Change Log:
-	2016-03-25: Initial Revision
-
-*/
 
 #ifndef SIMPLIFY_H_
 #define SIMPLIFY_H_
