3d5f9d2e60b94ae61fca6343a4559e075181c91a
bindukondreddi <bindukondreddi@gmail.com>
2017-06-05 21:05:11 -0400

    Update symboltable.h

--- src/symboltable.h
+++ src/symboltable.h
@@ -1,16 +1,3 @@
-/*
-
-	Name: Adrian J. Paparelli
-	Class: CSCI 5220
-	Session: Spring 2016
-
-	Description: symboltable.h defines all functions used in symbol names and AST trees
-		into a symbol table.
-
-	Change Log:
-	2016-02-26: Initial Revision
-
-*/
 
 #ifndef SYMBOLTABLE_H_
 #define SYMBOLTABLE_H_
